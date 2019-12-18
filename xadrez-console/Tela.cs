e72ce0bc4a8572a05272a0fc21274cc86ffa65a9
Fillipe van Keulen <fmvankeulen@gmail.com>
2019-12-18 11:19:46 -0300

    Criadas as classes Rei e Torre

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -11,10 +11,10 @@ namespace xadrez_console
             {
                 for (int j = 0; j < tab.colunas; j++)
                 {
-                    if (tab.peca(i, j) == null)
+                    if (tab.Peca(i, j) == null)
                         Console.Write("- ");
                     else
-                        Console.Write(tab.peca(i, j));
+                        Console.Write(tab.Peca(i, j));
                 }
                 Console.WriteLine();
             }            
